4ce2b5607b79e0c4977759b0ebfd0efe20120ca4
wongsatornxs <wongsatorn1999@outlook.com>
2023-07-02 16:30:55 +0700

    edit  interface project

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,6 +4,8 @@ import keys from "../keys";
 import "../App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCloud, faCloudRain } from "@fortawesome/free-solid-svg-icons";
+import dayjs from "dayjs";
+
 
 export default function Weather() {
   const api = keys;
@@ -13,9 +15,10 @@ export default function Weather() {
 
   const dateBuild = (d) => {
     let date = String(new window.Date());
-    date = date.slice(3, 15);
+    date = dayjs().format('ddd, MMM D, YYYY h:mm A ');
     return date;
   };
+ 
 
   function DayNameFromDate(dateStr) {
     const date = new Date(dateStr);
@@ -36,16 +39,12 @@ export default function Weather() {
 
         //get 5 day
         const { data: response } = await axios.get(
-          //   `${api.base}weather?q=Bangkok&units=metric&appid=${api.key}`
           `${api.BASE_URL}forecast?lat=${api.LAT}&lon=${api.LON}&appid=${api.API_KEY}`
         );
 
         let res = response.list;
         let today = new Date();
-        // const day = res.filter((item, index) => {
-        //   let date = new Date(item.dt_txt);
-        //   return date.toLocaleDateString() === today.toLocaleDateString();
-        // });
+      
         setWeather([]);
         // Next 5 Days
         for (let i = 0; i <= 4; i++) {
@@ -65,39 +64,52 @@ export default function Weather() {
     };
 
     fetchData();
+
+    const interval = setInterval(() => {
+      fetchData();
+    }, 60000);
+
+    // Clean up the interval when the component unmounts
+    return () => {
+      clearInterval(interval);
+    };
   }, []);
+  
 
   useEffect(() => {
     // console.log("Weather : ", weather);
   }, [weather]);
 
-  //   const filterDate = filterByDate(day, "2023-07-01");
-  //   const filterByDate = (list, dateString) => {
-  //     const filterDate = new Date(dateString);
-
-  //     return list.filter((item) => {
-  //       const itemDate = new Date(item.day);
-  //       return itemDate.getTime() === filterDate.getTime();
-  //     });
-  //   };
 
   return (
     weather &&
     weatherToday && (
       <div className="page">
         <div className="card-container">
-          <div className={weatherToday.main.temp > 18 ? "App hot" : "App cold"}>
+          <div className="App">
             {weatherToday != undefined ? (
               <div>
                 <div className="location-container">
                   <div className="location">
-                    {weatherToday.name}, {weatherToday.sys.country}
+                    {weatherToday.name}, {weatherToday.sys.country === "TH" ? (
+                     "Thailand"
+                    ): null}
                   </div>
                   <div className="date"> {dateBuild(new Date())}</div>
                 </div>
                 <div className="weather-container">
+                  <div className="set-icon">
+                  {weatherToday.weather[0].icon === "04d" ? (
+                    <FontAwesomeIcon icon={faCloud} size="6x" style={{color:"gray" }} />
+                  ) : weatherToday.weather[0].icon === "10d" ? (
+                    <FontAwesomeIcon icon={faCloudRain} size="6x" style={{color:"gray"}} />
+                  ) : null}
+                  </div>
                   <div className="temperature">
-                    {Math.round(weatherToday.main.temp)}°C
+                  <p class="max-w-lg text-7xl  font-black leading-normal text-zinc-50">
+                    {Math.round(weatherToday.main.temp)}° 
+                    </p>
+                    
                   </div>
                   <div className="weather">{weatherToday.weather[0].main}</div>
                 </div>
@@ -108,32 +120,32 @@ export default function Weather() {
           </div>
 
           <div className="forecast-header">
-            <p className="max-w-lg text-3xl font-semibold text-gray-600/100 dark:text-gray-500/100">
+            <p className="max-w-lg text-3xl font-semibold text-black-600/100 dark:text-black-500/100">
               Daily Forecast
             </p>
           </div>
-          <div className="grid gap-4">
-            <div>
-              <div className="grid grid-cols-5 gap-4">
-                {weather != undefined &&
-                  weather.map((row, index) => (
-                    <div className="col">
-                      {row.weather[0].icon === "04d" ? (
-                        <FontAwesomeIcon icon={faCloud} size="6x" />
-                      ) : row.weather[0].icon === "10d" ? (
-                        <FontAwesomeIcon icon={faCloudRain} size="6x" />
-                      ) : null}
-                      <div
-                        className="h-auto max-w-full rounded-lg"
-                        style={{ backgroundColor: "red" }}
-                      >
-                        {row.weather[0].description}
-                      </div>
-                      {DayNameFromDate(row.dt_txt)}
-                    </div>
-                  ))}
-              </div>
-            </div>
+
+          <div className="grid grid-cols-5 space-x-6 ">
+            {weather != undefined &&
+              weather.map((row, index) => (
+                <div className="bg-white p-1 shadow-md flex flex-col justify-center items-center rounded-lg">
+                 <div style={{paddingTop:'15px'}}>
+                  {row.weather[0].icon === "04d" ? (
+                    <FontAwesomeIcon icon={faCloud} size="6x" style={{color:"gray"}} />
+                  ) : row.weather[0].icon === "10d" ? (
+                    <FontAwesomeIcon icon={faCloudRain} size="6x" style={{color:"gray"}} />
+                  ) : null}
+                  </div>
+                  <div style={{paddingTop:'15px'}}>
+                  <p className="max-w-md text-lg font-semibold text-gray-600/100 dark:text-gray-500/100">
+                    {row.weather[0].description}
+                    </p>
+                  </div>
+                  <p className="max-w-md text-lg font-semibold text-gray-600/100 dark:text-gray-500/100">
+                  {DayNameFromDate(row.dt_txt)}
+                  </p>
+                </div>
+              ))}
           </div>
         </div>
       </div>
